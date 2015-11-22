0fb4fdc9b674c379730a9837b2b977af8f9ee252
Kurt Slagle <kurt_slagle@yahoo.com>
2015-11-22 12:08:18 -0600

    Create Pyramid.h

---
+++ project3/Pyramid.h
@@ -0,0 +1,50 @@
+ 
+// Pyramid.h
+
+#ifndef Pyramid_H
+#define Pyramid_H
+
+#include <GL/gl.h>
+
+#include "ModelViewWithPhongLighting.h"
+
+typedef float vec3[3];
+
+class Pyramid : public ModelViewWithPhongLighting
+{
+public:
+	Pyramid(float P1[3], float P2[3], float P3[3], float P4[3], float he, float c[]);
+
+	virtual ~Pyramid();
+
+	float colors[3];
+
+	cryph::AffPoint points[5];
+	float height;
+
+	cryph::AffVector upVector;
+
+	// xyzLimits: {mcXmin, mcXmax, mcYmin, mcYmax, mcZmin, mcZmax}
+	void getMCBoundingBox(double* xyzLimits) const;
+	void handleCommand(unsigned char key, double ldsX, double ldsY);
+	void render();
+
+private:
+	GLuint vao[1];
+	GLuint vbo[1];
+	GLuint ebo[3]; //The two ebos for the two triangle faces
+
+	float xmin, xmax, ymin, ymax, zmin, zmax;
+
+	vec3 normalVectorsVec3[5]; //The siz normals that I'll have to define myself with cross products
+	cryph::AffVector normals[5];
+
+	static GLuint indexList[3][3];
+
+	bool displayPyramidEdges, displayPyramidFill;
+
+	void definePyramid();
+	void renderPyramid(float* color);
+};
+
+#endif
