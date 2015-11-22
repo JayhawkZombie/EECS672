8f6a9ed4d566a307cfa34109a389a60beab1e6c8
Kurt Slagle <kurt_slagle@yahoo.com>
2015-11-22 13:03:42 -0600

    Create Gemstone.h

---
+++ project3/Gemstone.h
@@ -0,0 +1,48 @@
+// Gemstone.h
+
+#ifndef Gemstone_H
+#define Gemstone_H
+
+#include <GL/gl.h>
+
+#include "ModelViewWithPhongLighting.h"
+
+typedef float vec3[3];
+
+class Gemstone : public ModelViewWithPhongLighting
+{
+public:
+	Gemstone(float P1[3], float P2[3], float P3[3], float P4[3], float he, float c[]);
+
+	virtual ~Gemstone();
+
+	float colors[3];
+
+	cryph::AffPoint points[6];
+	float height;
+
+	cryph::AffVector upVector;
+
+	// xyzLimits: {mcXmin, mcXmax, mcYmin, mcYmax, mcZmin, mcZmax}
+	void getMCBoundingBox(double* xyzLimits) const;
+	void handleCommand(unsigned char key, double ldsX, double ldsY);
+	void render();
+
+private:
+	GLuint vao[1];
+	GLuint vbo[1];
+	GLuint ebo[7]; //We can draw 7 faces with ebos, but only 1 face with drawArrays :(
+
+	float xmin, xmax, ymin, ymax, zmin, zmax;
+
+	cryph::AffVector normals[8];
+
+	static GLuint indexList[7][3];
+
+	bool displayGemstoneEdges, displayGemstoneFill;
+
+	void defineGemstone();
+	void renderGemstone(float* color);
+};
+
+#endif
